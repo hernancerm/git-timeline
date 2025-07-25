6002599f8dfe2cda7ca555b32f19ec8a6dfef5b5
Hernán Cervera <hjcerveram@hotmail.com>
2025-07-25 16:08:22 -0500

    refactor: consolidate print calls

--- src/main/java/me/hernancerm/GitLogPrettyPrinter.java
+++ src/main/java/me/hernancerm/GitLogPrettyPrinter.java
@@ -4,13 +4,12 @@ import static org.fusesource.jansi.Ansi.ansi;
 
 public class GitLogPrettyPrinter {
 
-    // TODO: Colored output when expected (colored on terminal, not colored on non-TTY target).
     public void print(Commit commit) {
-        System.out.print(ansi().render("@|yellow " + commit.getAbbreviatedHash() + "|@ "));
-        System.out.print(ansi().render("@|green " + commit.getAuthorName() + "|@ "));
-        System.out.print(ansi().render("@|cyan " + commit.getAuthorDate() + "|@ "));
-        System.out.print(ansi().render(commit.getRefNamesColored()));
-        System.out.print(ansi().render(commit.getSubjectLine()));
-        System.out.println();
+        System.out.println(ansi().render(
+                "@|yellow " + commit.getAbbreviatedHash() + "|@"
+                        + " @|green " + commit.getAuthorName() + "|@"
+                        + " @|cyan " + commit.getAuthorDate() + "|@"
+                        + commit.getRefNamesColored()
+                        + " " + commit.getSubjectLine()));
     }
 }

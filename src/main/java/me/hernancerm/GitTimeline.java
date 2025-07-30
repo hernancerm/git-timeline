cbcad82eaf64e78b1bc8e56fae0c9a1460250ea4
Hernán Cervera <hjcerveram@hotmail.com>
2025-07-30 18:49:56 -0500

    fix: support help and version opts again

--- src/main/java/me/hernancerm/GitTimeline.java
+++ src/main/java/me/hernancerm/GitTimeline.java
@@ -1,20 +1,15 @@
 package me.hernancerm;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.fusesource.jansi.Ansi;
 
-import lombok.Data;
-
-//@Command(name = "git-timeline",
-//        version = "git-timeline 0.1",
-//        mixinStandardHelpOptions = true,
-//        description = "A small wrapper for git-log which improves readability.")
 public class GitTimeline implements Callable<Integer> {
+
+    private static final String NAME = "git-timeline";
+    private static final String VERSION = "0.1";
 
     private final String[] args;
     private final GitLogProcessBuilder gitLogProcessBuilder;
@@ -40,6 +35,14 @@ public class GitTimeline implements Callable<Integer> {
         GitLogArgs output = new GitLogArgs();
         for (String arg : args) {
             switch (arg) {
+                case "--help":
+                case "-h":
+                    handleHelpOption();
+                    break;
+                case "--version":
+                case "-v":
+                    handleVersionOption();
+                    break;
                 case "--color=always":
                     unparsedArgs.add(arg);
                     Ansi.setEnabled(true);
@@ -65,5 +68,23 @@ public class GitTimeline implements Callable<Integer> {
         }
         output.setUnparsedArgs(unparsedArgs.toArray(new String[0]));
         return output;
+    }
+
+    private void handleHelpOption() {
+        System.out.println(NAME + " " + VERSION);
+        System.out.println("""
+                A small wrapper for git-log which improves readability.
+                    -h, --help
+                        Display this help message. Since git-timeline is a wrapper around
+                        git-log, all options supported by git-log are also supported by
+                        git-timeline. See: git log --help
+                    -v, --version
+                        Display the version of git-timeline.""");
+        System.exit(0);
+    }
+
+    private void handleVersionOption() {
+        System.out.println(NAME + " " + VERSION);
+        System.exit(0);
     }
 }

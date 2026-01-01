e3f3b61015e326840a7ab1639def6662028a1c5b
Hernán Cervera <hjcerveram@hotmail.com>
2025-12-31 18:26:33 -0600

    release: 2.1

--- src/main/java/me/hernancerm/GitTimeline.java
+++ src/main/java/me/hernancerm/GitTimeline.java
@@ -9,7 +9,7 @@ import org.fusesource.jansi.Ansi;
 public class GitTimeline implements Callable<Integer> {
 
     private static final String NAME = "git-timeline";
-    private static final String VERSION = "2.1-SNAPSHOT";
+    private static final String VERSION = "2.1";
 
     private final String[] args;
     private final GitLogProcessBuilder gitLogProcessBuilder;
